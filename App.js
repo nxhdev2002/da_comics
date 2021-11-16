7b4424f7082702f79089ff5b698e3dffdce64e07
nxhdev2002 <xuanhoangv7c@gmail.com>
2021-11-16 22:51:09 +0700

    update

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {Picker} from '@react-native-picker/picker';
+
 import login from './apis/login'
+import get_follow_comics  from './apis/get_following_comics';
 
 const styles = StyleSheet.create({
   container: {
@@ -41,13 +45,57 @@ const styles = StyleSheet.create({
       textTransform: "uppercase"
   }
 });
+
+const getData = async (set_is_login, getemail, get_list_comics) => {
+  try {
+    const value = await AsyncStorage.getItem('is_login')
+    if (value !== '0') {
+      set_is_login('1')
+      email = await AsyncStorage.getItem('email')
+      getemail(email)
+      get_list_comics(await get_follow_comics("1"))
+    } else {
+      set_is_login('0')
+    }
+  } catch(e) {
+    set_is_login('0')
+  }
+}
+
+
 const App = () => {
-  return (
-    <View>
-       <TouchableOpacity style={styles.appButtonContainer} onPress={() => login("xuanhoangv7c@gmail.com", "Caube_2k2")}>
-            <Text style={styles.appButtonText}>Tiếp tục</Text>
-        </TouchableOpacity>
-    </View>
-  )
+  const [check_login, set_is_login] = useState(0)
+  const [email, getemail] = useState('')
+  const [list_comics, get_list_comics] = useState([])
+  useEffect(() => {
+    getData(set_is_login, getemail, get_list_comics)
+  }, [])
+  if (check_login == '0') {
+    return (
+      <View>
+         <TouchableOpacity style={styles.appButtonContainer} onPress={() => login("xuanhoangv7c@gmail.com", "Caube_2k2")}>
+              <Text style={styles.appButtonText}>Tiếp tục</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  } else {
+    return (
+      <View>
+         <Text>Logged in with {email}. List truyện: </Text>
+              <Picker
+                style={{
+                  height: 36,
+                  width: 261,
+                }}
+              >
+                <Picker.Item key={-1} label={'Search By...'} value="first" />
+                <Picker.Item label="hoangdz" />
+              </Picker>
+         <TouchableOpacity style={styles.appButtonContainer} onPress={async () => await AsyncStorage.setItem('is_login', '0')}>
+              <Text style={styles.appButtonText}>Log Out</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
 }
 export default App;
--- apis/login.js
+++ apis/login.js
@@ -1,11 +1,12 @@
 import axios from "axios"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import RNRestart from 'react-native-restart';
 
 const parse_require_data = (source) => {
     const regex = /input type="hidden" name="(.*)" .+ value="(.*)"/gm;
     let m;
     let arr = []
     while ((m = regex.exec(source)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
         let temp = []
         temp.push(m[1])
         temp.push(m[2])
@@ -14,6 +15,18 @@ const parse_require_data = (source) => {
     const entries = new Map(arr);
     const obj = Object.fromEntries(entries);
     return (obj);
+}
+
+const set_user_info = async (source) => {
+    const regex = /uid='(.*)';.+Key='(.*)'.+token='(.*)'.+Name='(.*)';.+email='(.*)'/gm;
+    let m;
+    while ((m = regex.exec(source)) !== null) {
+        await AsyncStorage.setItem("userGuid", m[1])
+        await AsyncStorage.setItem("userKey", m[2])
+        await AsyncStorage.setItem("token", m[3])
+        await AsyncStorage.setItem("fullName", m[4])
+        await AsyncStorage.setItem("email", m[5])
+    }
 }
 const default_login = async (user, pass) => {
     const url = "https://www.nettruyenpro.com/Secure/Login.aspx?returnurl=%2f"
@@ -27,7 +40,12 @@ const default_login = async (user, pass) => {
     }   
     const post_data = new URLSearchParams(Object.assign(token,require_data));
     const rs = await axios.post(url, post_data)
+    const get_auth = await axios.get("http://www.nettruyenpro.com/Comic/Services/ComicService.asmx/GetAuth")
+    await set_user_info(get_auth.data.data)
+    await AsyncStorage.setItem("is_login", '1')
+    RNRestart.Restart()
 }
+
 const login = (user, password, method="default") => {
     switch(method) {
         case "google":
