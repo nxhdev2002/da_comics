24f200221a5eef5509910f1dff94f6d37f68820b
nxhdev2002 <xuanhoangv7c@gmail.com>
2022-02-23 00:28:47 +0700

    update

--- Components/InfoComicScreen.js
+++ Components/InfoComicScreen.js
@@ -1,12 +1,13 @@
 import React, {useEffect, useState} from "react"
-import {Text, View, TextInput, TouchableWithoutFeedback, Dimensions} from 'react-native'
+import {Text, View, TextInput, Button, Alert, TouchableWithoutFeedback, Dimensions} from 'react-native'
 import Image from 'react-native-scalable-image'
 import { ScrollView } from "react-native-gesture-handler"
 import getAdultComics from '../apis/get_comic'
 
 const infoComicScreen = ({route, navigation}) => {
+    const [chapter, setChapter] = useState('')
     const myloop = [];
-    for (let i = 1; i <= route.params.newchapter; i++) {
+    for (let i = route.params.newchapter; i > 0; i--) {
         myloop.push(
             <TouchableWithoutFeedback key={i} onPress={() => navigation.navigate('reading_screen', {chapter: i, url: route.params.url})}>
                     <Text style={{padding: 15, borderBottomColor: 'red', borderBottomWidth: 0.5}}>Chapter {i}</Text>
@@ -14,8 +15,8 @@ const infoComicScreen = ({route, navigation}) => {
         );
     }
     return (
-        <View>
-            <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+        <View style={{flex: 1}}>
+            <View style={{flex: 3, flexDirection: 'row', justifyContent: 'center'}}>
                 <Image
                     width={Dimensions.get('window').width}
                     height={151} // height will be calculated automatically
@@ -23,14 +24,41 @@ const infoComicScreen = ({route, navigation}) => {
                 />
             </View>
             <Text style={{alignSelf: 'center', marginTop: '3%'}}>{route.params.desc}</Text>
-            <View style={{height: '76%'}}>
+            <View style={{flex: 8}}>
                 <Text>Chapter List</Text>
                 <ScrollView>
                     {myloop}
                 </ScrollView>
-                <TextInput
-                />
             </View>
+            <View style={{flex: 1,flexDirection:'row', flexWrap:'wrap', justifyContent: 'space-around', alignItems:'center', height: '10%'}}>
+                <TextInput
+                    placeholder="Nhập chapter cần đọc"
+                    keyboardType="numeric"
+                    contextMenuHidden={true}
+                    onChangeText={(text) => {setChapter(text)}}
+                /><Button
+                title="Đọc truyện"
+                onPress={() => {
+                    if (Number(chapter) <= 0 || Number(chapter) > route.params.newchapter) {
+                        Alert.alert(
+                            "Lỗi",
+                            "Có vẻ như bạn nhập chapter có vấn đề. Bạn có muốn đọc từ chap 1 không?",
+                            [
+                              {
+                                text: "Cancel",
+                                onPress: () => console.log("Cancel Pressed"),
+                              },
+                              { 
+                                text: "OK", onPress: () => navigation.navigate('reading_screen', {chapter: '1', url: route.params.url}) 
+                              }
+                            ]
+                        );
+                    } else {
+                        navigation.navigate('reading_screen', {chapter: chapter, url: route.params.url})
+                    }
+                }}
+              />
+                </View>
         </View>
     )
 }
--- Components/SplashScreen.js
+++ Components/SplashScreen.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
-import {Text, View} from 'react-native'
+import {View, Text, Dimensions} from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import Image from 'react-native-scalable-image'
 
 const SplashScreen = ({navigation}) => { 
   const [is_loading, set_loading_status] = useState(true);
@@ -8,7 +9,7 @@ const SplashScreen = ({navigation}) => {
   const get_data = async () => {
     const rs = await AsyncStorage.getItem('is_login')
     set_loading_status(false)
-    setData(rs)
+    setTimeout(() => {setData(rs)}, 1000)
   }
   useEffect(() => {
     get_data()
@@ -20,19 +21,21 @@ const SplashScreen = ({navigation}) => {
       }
   }, [data])
   const viewStyles = [
-    { backgroundColor: 'orange' }
+  { backgroundColor: 'white',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
   ];
-  const textStyles = {
-    color: 'white',
-    fontSize: 40,
-    fontWeight: 'bold'
-  };
-
   return (
     <View style={viewStyles}>
-      <Text style={textStyles}>
-        Splash Screen
-      </Text>
+      <Image
+        width={Dimensions.get('window').width}
+        style={{borderRadius: Dimensions.get('window').width / 2}}
+        // height will be calculated automatically
+        source={{uri: 'https://scontent.fhan3-5.fna.fbcdn.net/v/t39.30808-6/273579511_3027156484279711_1844956534843447760_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=e3f864&_nc_ohc=WMZus5z2LmkAX-H-ctc&_nc_ht=scontent.fhan3-5.fna&oh=00_AT_NX4aGHyw3KC0j05KvR9cdnjEinyzHV4fLm6ux-oFDjg&oe=62191F16'}}
+      />
+      <Text style={{padding: 50}}>DA Comics by nxhdev2002</Text>
     </View>
   );
 }
